4f9370bd0e35bd73ab253578a9b72865fe7fa6bc
Adrian Wyatt <Adi.Wyatt@outlook.com>
2016-04-11 15:52:47 -0400

    required mongoose and pointed mongoose.connect to localhost plus console log connection errors

--- db/connection.js
+++ db/connection.js
@@ -1,4 +1,13 @@
 var mongoose = require("mongoose");
+mongoose.connect("mongod://localhost/3001");
+
+db.on("error", function(error){
+  console.log(error);
+});
+
+db.once("open", function(){
+  console.log("You are connected!!")
+});
 
 var seedData = require("./seeds.json");
 module.exports = {
