b5acc864e1323fcf0fe9af599b9e77a029c17f9b
Toni Langley <chibia2@gmail.com>
2016-04-12 17:30:55 -0400

    angular classwork

--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,69 @@
 "use strict";
 
 (function(){
-  
+
   angular
   .module("candidates", [
     "ui.router",
     "ngResource"
+  ])
+  .config([
+    "$stateProvider",
+    "$locationProvider",
+    "$urlRouterProvider",
+    Router
+  ])
+  .factory("Candidate",[
+    "$resource",
+    Candidate
+  ])
+  .controller("candIndexCtrl", [
+    "Candidate",
+    candIndexCtrl
+  ])
+  .controller("candShowCtrl", [
+    "$stateParams",
+    candShowCtrl
   ]);
+
+  function Router($stateProvider, $locationProvider, $urlRouterProvider){
+    $locationProvider.html5Mode(true);
+    $stateProvider
+    .state("welcome", {
+      url: "/",
+      templateUrl: "/assets/html/app-welcome.html"
+    })
+    .state("index", {
+      url: "/candidates",
+      templateUrl: "/assets/html/candidates-index.html",
+      controller: "candIndexCtrl",
+      controllerAs: "indexVM"
+    })
+    .state("show", {
+      url: "/candidates/:name",
+      templateUrl: "/assets/html/candidates-show.html",
+      controller: "candShowCtrl",
+      controllerAs: "showVM"
+    });
+    $urlRouterProvider.otherwise("/");
+  }
+
+  function Candidate($resource){
+    var Candidate = $resource("/api/candidates/:name", {}, {
+      update: {method: "PUT"}
+    });
+    Candidate.all = Candidate.query();
+    
+    return Candidate;
+  }
+
+  function candIndexCtrl(Candidate){
+    var vm = this;
+    vm.candidates = Candidate.all;
+  }
+
+function candShowCtrl(Candidate, $stateParams){
+  var vm = this;
+}
 
 })();
