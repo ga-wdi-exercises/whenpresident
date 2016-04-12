440aa04a05a29b91090f10694f8319c9f80b68bc
Melissa Miller <lis.d.miller@gmail.com>
2016-04-11 20:43:41 -0400

    Follows Express Mongoose lesson

--- db/connection.js
+++ db/connection.js
@@ -1,4 +1,13 @@
-var seedData = require("./seeds.json");
-module.exports = {
-  candidates: seedData
-};
+var mongoose = require("mongoose");
+
+var CandidateSchema = new mongoose.Schema(
+  {
+    name: String,
+    year: Number
+  }
+); // end candidate schema
+
+mongoose.model("Candidate", CandidateSchema);
+mongoose.connect("mongodb://localhost/whenpresident");
+
+module.exports = mongoose;
