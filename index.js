061081736b70019f4c5222ba0a2e91c6315f33f4
jblakeman <blakeman.john@gmail.com>
2016-04-12 19:14:24 -0400

    Use variable for path; fix text editor tab/space aligns

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ var express   = require("express");
 var parser    = require("body-parser");
 var hbs       = require("express-handlebars");
 var Candidate = require("./db/connection").CandidateModel;
-var ctrl 	    = require("./route-controllers");
+var ctrl      = require("./route-controllers");
 var app       = express();
+
+var candidatesPath = "/candidates";
 
 app.set("port", process.env.PORT || 3001);
 app.set("view engine", "hbs");
@@ -18,24 +20,24 @@ app.use(parser.urlencoded({extended: true}));
 
 app.get("/", ctrl.rootCtrl);
 
-app.get("/candidates", function(req, res) {
-	ctrl.candidateIndexCtrl(req, res, Candidate);
+app.get(candidatesPath, function(req, res) {
+  ctrl.candidateIndexCtrl(req, res, Candidate);
 });
 
-app.post("/candidates", function(req, res) {
-	ctrl.candidateNewCtrl(req, res, Candidate);
+app.post(candidatesPath, function(req, res) {
+  ctrl.candidateNewCtrl(req, res, Candidate);
 });
 
-app.get("/candidates/:name", function(req, res) {
-	ctrl.candidateShowCtrl(req, res, Candidate);
+app.get(candidatesPath + "/:name", function(req, res) {
+  ctrl.candidateShowCtrl(req, res, Candidate);
 });
 
-app.post("/candidates/:name/delete", function(req, res) {
-	ctrl.candidateDeleteCtrl(req, res, Candidate);
+app.post(candidatesPath + "/:name/delete", function(req, res) {
+  ctrl.candidateDeleteCtrl(req, res, Candidate);
 });
 
-app.post("/candidates/:name/", function(req, res) {
-	ctrl.candidateEditCtrl(req, res, Candidate);
+app.post(candidatesPath + "/:name/", function(req, res) {
+  ctrl.candidateEditCtrl(req, res, Candidate);
 });
 
 app.listen(app.get("port"), function(){
