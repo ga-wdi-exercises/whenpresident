585b67a9cf3d51b7a3c42e1b55284615dc5152eb
Max Kaye <maximillianchina@gmail.com>
2016-04-12 15:54:12 -0400

    added env.json

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,23 @@
 console.log("Working!");
+"use strict";
+
+(function(){
+  angular
+  .module("candidates", [
+    "ui.router",
+    "ngResource"
+  ])
+  .config([
+    "$stateProvider",
+    Router
+  ]);
+
+  function Router($stateProvider){
+    $stateProvider
+    .state("welcome", {
+      url: "/",
+      templateUrl: "/assets/html/candidates-welcome.html"
+    });
+
+  }
+})();
