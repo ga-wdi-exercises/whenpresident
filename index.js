d5304560d25cd9afc58bee322cd5a51fa174da65
Woo Yun <hh747hh@gmail.com>
2016-04-11 23:32:56 -0400

    only show delete buttonon show page

--- index.js
+++ index.js
@@ -37,12 +37,25 @@ app.get("/candidates/:name", function(req, res){
 });
 
 app.post("/candidates", function(req, res){
-
-
   Candidate.create(req.body.candidate).then(function(candidate){
     res.redirect("/candidates/"+candidate.name);
-  })
+  });
 });
+
+
+app.post("/candidates/:name/delete", function(req, res){
+  Candidate.findOneAndRemove({name: req.params.name}).then(function(){
+    res.redirect("/candidates")
+  });
+});
+
+
+app.post("/candidates/:name", function(req, res){
+  Candidate.findOneAndUpdate({name: req.params.name}, req.body.candidate, {new: true}).then(function(candidate){
+    res.redirect("/candidates/" + candidate.name);
+  });
+});
+
 app.listen(app.get("port"), function(){
   console.log("It's aliiive!");
 });
