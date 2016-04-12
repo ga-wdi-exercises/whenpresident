4d75e801635837a16ef6d6d33fe6f406009613ea
YOUR FULL NAME <farishta.haider@gmail.com>
2016-04-12 14:50:25 -0400

    hw

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 var express = require("express");
+var parser = require("body-parser");
 var hbs     = require("express-handlebars");
-var db      = require("./db/connection");
+var mongoose = require("./db/connection");
 
 var app     = express();
+
+var Candidate = mongoose.model("Candidate");
 
 app.set("port", process.env.PORT || 3001);
 app.set("view engine", "hbs");
@@ -13,27 +16,25 @@ app.engine(".hbs", hbs({
   defaultLayout:  "layout-main"
 }));
 app.use("/assets", express.static("public"));
+app.use(parser.urlencoded({extended :true}));
 
 app.get("/", function(req, res){
   res.render("app-welcome");
 });
 
 app.get("/candidates", function(req, res){
-  res.render("candidates-index", {
-    candidates: db.candidates
+  Candidate.find({}).then(function(){
+    res.render("candidates-index", {
+      candidates: candidates
+    });
   });
 });
 
 app.get("/candidates/:name", function(req, res){
-  var desiredName = req.params.name;
-  var candidateOutput;
-  db.candidates.forEach(function(candidate){
-    if(desiredName === candidate.name){
-      candidateOutput = candidate;
-    }
-  });
-  res.render("candidates-show", {
-    candidate: candidateOutput
+  Candidate.findOne({name: req.params.name}).then(function(candidate){
+    res.render("candidates-show", {
+      candidate: candidate
+    });
   });
 });
 
