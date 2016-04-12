35e31bb6372d12dd9bd530d27d55a6fd3de32be3
Cam <camlatimer@gmail.com>
2016-04-12 15:43:05 -0400

    configured app router and defined welcome state

--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,24 @@
 "use strict";
 
 (function(){
-  
+
   angular
   .module("candidates", [
     "ui.router",
     "ngResource"
   ]);
+  //configure the router
+  .config([
+    "$stateProvider",
+    Router
+  ])
+  function Router ($stateProvider){
+    $stateProvider
+    .state("welcome", {
+      url: "/",
+      templateUrl: "/assets/html/app-welcome.html"
+      //templates are specified via app.use("/assets", express.static("public")); in index.js
+    });
+  };
 
 })();
