78fb56ee53d7649b197e16dda5cf261078fe42c4
ALEXA KLEIN <lexi.klein1@gmail.com>
2016-04-12 09:53:52 -0400

    from class and the review

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 var express = require("express");
 var hbs     = require("express-handlebars");
-var db      = require("./db/connection");
+var mongoose = require("./db/connection");
+// var db      = require("./db/connection"); **rename db to mongoose
 
 var app     = express();
+
+var Candidate = mongoose.model("Candidate");
 
 app.set("port", process.env.PORT || 3001);
 app.set("view engine", "hbs");
@@ -12,28 +15,67 @@ app.engine(".hbs", hbs({
   layoutsDir:     "views/",
   defaultLayout:  "layout-main"
 }));
+
 app.use("/assets", express.static("public"));
+app.us(parser.urlencoded({extended:true}));
 
 app.get("/", function(req, res){
   res.render("app-welcome");
 });
 
 app.get("/candidates", function(req, res){
-  res.render("candidates-index", {
-    candidates: db.candidates
+    Candidate.find({}.then(function(candidates){  //returned_candidates
+      res.render("candidates-index",{
+        candidates: candidates  //returned_candidates
+      });
+    });
+  });
+  //   res.render("candidates-index", {   **these lines were replaced by 26-28
+  //     candidates: db.candidates
+  //     });
+  //   });
+  // });
+
+app.get("/candidates/:name", function(req, res){
+  Candidate.findOne({name: req.params.name}).then(function(){
+    res.render("candidates-show", {
+      candidate: candidate
+    });
   });
 });
 
-app.get("/candidates/:name", function(req, res){
-  var desiredName = req.params.name;
-  var candidateOutput;
-  db.candidates.forEach(function(candidate){
-    if(desiredName === candidate.name){
-      candidateOutput = candidate;
-    }
+  // var desiredName = req.params.name;   **these lines replaces by 39-41
+  // var candidateOutput;
+  // db.candidates.forEach(function(candidate){
+  //   if(desiredName === candidate.name){
+  //     candidateOutput = candidate;
+  //   }
+  // });
+  // res.render("candidates-show", {
+  //   candidate: candidateOutput
+  // });
+// });
+
+app.post("/candidates", function(req, res){
+  // res.json(req.body);
+  var candidateData = req.body.candidate;
+  Candidate.create(candidateData).then(function(newCandidate){
+    res.redirect("/candidates/" + candidate.name);
   });
-  res.render("candidates-show", {
-    candidate: candidateOutput
+});
+
+app.post("/candidates/:name", function(req, res){
+  var desiredName = req.params.name
+  var candidateData = req.body.candidate;
+  Candidate.findOneAndUpdate(name: desiredName), candidateData, {new: true}).then(function(updatedCandidate){
+    res.redirect("/candidates/" + updatedCandidate.name);
+  });
+});
+
+app.post("/candidates/:name/delete", function(req, res){
+  var desiredName = req.params.name
+  Candidate.findOneAndRemove(name: desiredName).then(function(){
+    res.redirect("/candidates/");
   });
 });
 
