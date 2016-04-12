0755282273000a72d36df749476bbf510558a27e
Caitlin Daitch <caitlin.daitch@gmail.com>
2016-04-12 16:52:19 -0400

    Refactoring backend into angular frontend, adjusting URLs

--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,54 @@
 "use strict";
 
 (function(){
-  
+
   angular
   .module("candidates", [
     "ui.router",
     "ngResource"
-  ]);
+  ])
+  .config([
+    "$stateProvider",
+    "$locationProvider",
+    "$urlRouterProvider",
+    Router
+  ])
+  .factory("Candidate", [
+    "$resource",
+    Candidate
+  ])
+  .controller("candIndexCtrl", [
+    "Candidate",
+    candIndexCtrl
+  ])
+
+  function Router($stateProvider, $locationProvider, $urlRouterProvider){
+    $locationProvider.html5Mode(true)
+    $stateProvider
+    .state("welcome", {
+      url: "/",
+      templateUrl: "/assets/html/candidates-welcome.html"
+    })
+    .state("index", {
+      url: "/candidates",
+      templateUrl: "/assets/html/candidates-index.html",
+      controller: "candIndexCtrl",
+      controllerAs: "indexVM"
+    })
+    $urlRouterProvider.otherwise("/");
+  }
+
+  function Candidate($resource){
+    var Candidate = $resource("/api/candidates/:name", {}, {
+      update: {method: "PUT"}
+    });
+    Candidate.all = Candidate.query();
+    return Candidate
+  }
+
+  function candIndexCtrl(candidate){
+    var vm = this;
+    vm.candidates = Candidate.all;
+  }
 
 })();
