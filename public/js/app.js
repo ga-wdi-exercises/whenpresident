44de7ca25f36138a6c241ea790ffb4d8530d8a66
Maureen VOGEL <maureenvogel@Maureen-VOGELs-MacBook-Pro.local>
2016-04-12 17:04:31 -0400

    made angular changes

--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,54 @@
 "use strict";
 
 (function(){
-  
+
   angular
   .module("candidates", [
     "ui.router",
     "ngResource"
+  ])
+
+  .config([
+    "$stateProvider",
+    "$locationProvider",
+    "$urlRouterProvider",
+    Router
+  ])
+  .factory("Candidate", [
+    "$resource",
+    Candidate
+  ])
+  .controller("candIndexCtrl", [
+    "Candidate",
+    candIndexCtrl
   ]);
 
+  function Router ($stateProvider, $locationProvider, $urlRouterProvider){
+    $locationProvider.html5Mode(true) // gets rid of the /#/ in angular
+    $stateProvider
+    .state("welcome", {
+      url: "/",
+      templateUrl: "/assets/html/candidates-welcome.html"
+    })
+    .state("index", {
+      url: "/candidates",
+      templateUrl: "/assets/html/candidates-index.html",
+      controller: "candIndexCtrl",
+      controllerAs: "indexVM"
+    });
+    $urlRouterProvider.otherwise("/");
+  }
+
+  function Candidate($resource){
+    var Candidate = $resource("/api/candidates/:name", {}, {
+      update: {method: "PUT"}
+    });
+    Candidate.all = Candidate.query();
+    return Candidate;
+  }
+
+  function candIndexCtrl(Candidate){
+    var vm = this;
+    vm.candidates = Candidate.all;
+  }
 })();
