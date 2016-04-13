6b70aff9e2d001ce9da7d4279556d3ee2e4781af
Christine Movius <christine.movius@gmail.com>
2016-04-12 20:51:14 -0400

    Add welcome and index states in angular.

--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,26 @@
 "use strict";
 
 (function(){
-  
   angular
   .module("candidates", [
     "ui.router",
     "ngResource"
+  ])
+  .config([
+    "$stateProvider",
+    Router
   ]);
+
+  function Router($stateProvider){
+    $stateProvider
+    .state("welcome", {
+      url: "/",
+      templateUrl: "/assets/html/app-welcome.html" //why is this .html instead of .hbs?
+    })
+    .state("index", {
+      url: "/candidates",
+      templateUrl: "/assets/html/candidates-index.html"
+    });
+  }
 
 })();
