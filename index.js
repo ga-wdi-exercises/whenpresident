27808546c68729807e61c747594eabdcc0d5301b
Caitlin Daitch <caitlin.daitch@gmail.com>
2016-04-13 10:15:40 -0400

    Candidates show working

--- index.js
+++ index.js
@@ -38,6 +38,7 @@ app.engine(".hbs", hbs({
 }));
 app.use("/assets", express.static("public"));
 app.use(parser.urlencoded({extended: true}));
+app.use(parser.json({extended: true}));
 app.use(function(req, res, next){
   twitter.checkIfSignedIn(req, res, function(){
     next();
--- public/js/app.js
+++ public/js/app.js
@@ -74,6 +74,11 @@
     Candidate.find("name", $stateParams.name, function(candidate){
       vm.candidate = candidate;
     });
+    vm.update = function(){
+      Candidate.update({name: vm.candidate.name}, {candidate: vm.candidate}, function(){
+        console.log("Done updating!");
+      });
+    }
   }
 
 })();
