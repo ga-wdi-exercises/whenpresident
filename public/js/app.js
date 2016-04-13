401e21b0f215f809eca5a9060acc035f72c72911
YOUR FULL NAME <farishta.haider@gmail.com>
2016-04-13 14:25:12 -0400

    Site comes up, can sign in w twitter but can't get to candidates page

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,6 @@
 "use strict";
 
 (function(){
-
   angular
   .module("candidates", [
     "ui.router",
@@ -14,8 +13,6 @@
     "$urlRouterProvider"
     Router
   ]);
-  .factory("Candidate", [
-
     .controller("canIndexCtrl", [
       "Candidate",
       canIndexCtrl
@@ -26,50 +23,50 @@
       canShowCtrl
     ]);
     function Router($stateProvider, $locationProvider, $urlRouterProvider){
-    $locationProvider.html5mode(true);
-    $stateProvider
-    .state("welcome", {
-      url: "/",
-      templateUrl: "/assets/html/candidates-welcome.html"
-    })
-    .state("index", {
-      url: "/candidates",
-      template:"<h2> This is the candidates index page.</h2>"
-    });
-    .state("show", {
-      url: "candidates/:name",
-      templateUrl: "/assets/html/candidates-show.html",
-      controller: "canShowCtrl",
-      controllerAs: "showVM"
-    });
-    $urlRouterProvider.otherwise("/");
-  }
-      update: {method: "PUT"}
-    });
-    Candidate.all = Candidate.query();
-    Candidate.find = function(property, value, callback){
-      Candidate.all.$promise.then(function(){
-        Candidate.all.forEach(function(){
-          if(candidate[property] == value) callback(candidate);
-        });
-      });
-    }
-    return Candidate;
-  }
-  function canIndexCtrl(Candidate){
-    var vm = this;
-    vm.candidates = Candidate.all;
-  }
-
-  function(canShowCtrl(Candidate, $stateParams){
-    var vm = this;
-    Candidate.find("name", $stateParams.name, function(candidate){
-      vm.candidate = candidate;
-    });
-    vm.update = function(){
-      Candidate.update({name: vm.candidate.name}, {candidates: vm.candidate}, function(){
-        console.log("DONE!!!!!!");
+      $locationProvider.html5mode(true);
+      $stateProvider
+      .state("welcome", {
+        url: "/",
+        templateUrl: "/assets/html/candidates-welcome.html"
       })
+      .state("index", {
+        url: "/candidates",
+        template:"<h2> This is the candidates index page.</h2>"
+      });
+      .state("show", {
+        url: "candidates/:name",
+        templateUrl: "/assets/html/candidates-show.html",
+        controller: "canShowCtrl",
+        controllerAs: "showVM"
+      });
+      $urlRouterProvider.otherwise("/");
     }
+    update: {method: "PUT"}
+  });
+  Candidate.all = Candidate.query();
+  Candidate.find = function(property, value, callback){
+    Candidate.all.$promise.then(function(){
+      Candidate.all.forEach(function(){
+        if(candidate[property] == value) callback(candidate);
+      });
+    });
   }
+  return Candidate;
+}
+function canIndexCtrl(Candidate){
+  var vm = this;
+  vm.candidates = Candidate.all;
+}
+
+function(canShowCtrl(Candidate, $stateParams){
+  var vm = this;
+  Candidate.find("name", $stateParams.name, function(candidate){
+    vm.candidate = candidate;
+  });
+  vm.update = function(){
+    Candidate.update({name: vm.candidate.name}, {candidates: vm.candidate}, function(){
+      console.log("DONE!!!!!!");
+    })
+  }
+}
 })();
