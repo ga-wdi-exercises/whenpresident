17ec3127a40fdcfe1e5c137f7cce84af1aa7b46f
winishasmith <winishasmith@gmail.com>
2016-04-12 20:26:30 -0400

    inclass code along edits

--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,38 @@
 "use strict";
 
 (function(){
-  
+
   angular
   .module("candidates", [
     "ui.router",
     "ngResource"
+  ])
+  .config([
+    "$stateProvider",
+    Router
+  ])
+  .factory("Candidate", [
+    "$resource",
+    Candidate
+  ])
+  .controller("canIndexCtrl", [
+    "Candidate",
+    candIndexCtrl
   ]);
+
+
+  function Router($stateProvider){
+    $stateProvider
+    .state("welcome", {
+      url: "/",
+      templateUrl: "/assets/html/app-welcome.html"
+    })
+    .state("index", {
+      url: "/candidates",
+      templateUrl: "/assets/html/candidates-index.html",
+      controller: "candIndexCtrl",
+      controllerAs: "indexVM"
+    });
+  }
 
 })();
