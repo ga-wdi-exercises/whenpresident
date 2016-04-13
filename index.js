4dc49a429e03bb26eff64349b87e71264daa088a
Crucnhwrap78 <Hayesapples78@gmail.com>
2016-04-13 14:43:50 -0400

    added angular partially

--- index.js
+++ index.js
@@ -7,10 +7,10 @@ var qstring = require("qs");
 var mongoose= require("./db/connection");
 var twitter = require("./lib/twitter_auth");
 
-var app     = express();
+var app = express();
 
 var Candidate = mongoose.model("Candidate");
-
+//runs the process to login with twitter
 if(process.env.NODE_ENV !== "production"){
   var env   = require("./env");
   process.env.session_secret = env.session_secret;
@@ -44,10 +44,6 @@ app.use(function(req, res, next){
   });
 });
 
-app.get("/", function(req, res){
-  res.render("candidates");
-});
-
 app.get("/login/twitter", function(req, res){
   twitter.getSigninURL(req, res, function(url){
     res.redirect(url);
@@ -60,51 +56,38 @@ app.get("/login/twitter/callback", function(req, res){
   });
 });
 
-app.get("/candidates", function(req, res){
+app.get("/logout", function(req, res){
+  req.session.destroy();
+  res.redirect("/");
+});
+
+app.get("api/candidates", function(req,res){
   Candidate.find({}).then(function(candidates){
-    res.render("candidates-index", {
-      candidates: candidates
-    });
+    res.json(candidates);
   });
 });
 
-app.get("/candidates/:name", function(req, res){
+app.get("/api/candidates/:name", function(req, res){
   Candidate.findOne({name: req.params.name}).then(function(candidate){
-    res.render("candidates-show", {
-      candidate: candidate,
-      isCurrentUser: (candidate._id == req.session.candidate_id)
-    });
+    candidate.isCurrentUser = (candidate._id == req.session.candidate_id);
+    res.json(candidate);
   });
 });
 
-app.post("/candidates/:name/delete", function(req, res){
+app.delete("/api/candidates/:name", function(req, res){
   Candidate.findOneAndRemove({name: req.params.name}).then(function(){
-    res.redirect("/candidates")
+     res.json({success: true});
   });
 });
 
-app.post("/candidates/:name", function(req, res){
+app.put("/api/candidates/:name", function(req, res){
   Candidate.findOneAndUpdate({name: req.params.name}, req.body.candidate, {new: true}).then(function(candidate){
-    res.redirect("/candidates/" + candidate.name);
+    res.json(candidate);
   });
 });
 
-app.post("/candidates/:name/positions", function(req, res){
-  Candidate.findOne({name: req.params.name}).then(function(candidate){
-    candidate.positions.push(req.body.position);
-    candidate.save().then(function(){
-      res.redirect("/candidates/" + candidate.name);
-    });
-  });
-});
-
-app.post("/candidates/:name/positions/:index", function(req, res){
-  Candidate.findOne({name: req.params.name}).then(function(candidate){
-    candidate.positions.splice(req.params.index, 1);
-    candidate.save().then(function(){
-      res.redirect("/candidates/" + candidate.name);
-    });
-  });
+app.get("/*", function(req, res){
+  res.render("candidates");
 });
 
 app.listen(app.get("port"), function(){
--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,48 @@
 "use strict";
 
 (function(){
-  
+
   angular
   .module("candidates", [
     "ui.router",
     "ngResource"
-  ]);
+  ])
+  .config([
+    "$stateProvider",
+    "$locationProvider",
+    "$urlRouterProvider"
+    Router
+  ])
+  .factory("Candidate", [
+    candidIndexController
+  ])
 
+  function Router($stateProvider, $locationProvider, $urlRouterProvider){
+    $locationProvider.html5Mode(true);
+    $stateProvider
+    .state("welcome", {
+      url: "/",
+      templateUrl: "/assets/html/candidates-welcome"
+    })
+    .state("index", {
+      url: "/candidates",
+      templateUrl: "assets/html/candidates-index.html",
+      controller: "candidIndexController";
+      controllerAs: "indexVM"
+    });
+    $urlRouterProvider.otherwise("/")
+  }
+
+  function Candidate($resource){
+    var Candidate = $resource("/api/candidates/:name", {}, {
+      update: {method: "PUT"}
+    });
+    Candidate.all = Candidate.query();
+    return Candidate;
+  }
+
+  function candIndexCtrl(Candidate){
+    var vm = this;
+    vm.candidates = Candidate.all;
+  }
 })();
