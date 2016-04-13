ba10ddb93f0b07fd3a1f826cda7560c3b70384bd
Melissa Miller <lis.d.miller@gmail.com>
2016-04-13 13:52:44 -0400

    in class lesson

--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,54 @@
 "use strict";
 
 (function(){
-  
+
   angular
   .module("candidates", [
     "ui.router",
     "ngResource"
+  ])
+  .config([
+    "$stateProvider",
+    "$locationProvider",
+    "$urlRouterProvider".
+    Router
+  ])
+  .factory("CandidateFactory", [
+    "$resource",
+    CandidateFactory
+  ])
+  .controller("candIndexCtrl", [
+    "CandidateFactory",
+    candIndexCtrl
   ]);
+
+  function Router($stateProvider, $locationProvider, $urlRouterProvider){
+    $locationProvider.html5Mode(true);
+    $stateProvider
+    .state("welcome", {
+      url: "/",
+      templateUrl: "/assets/html/candidates-welcome.html"
+    })
+    .state("index", {
+      url: "/candidates",
+      templateUrl: "/assets/html/candidates-index.html",
+      controller: "candIndexCtrl",
+      controllerAs: "indexVM"
+    });
+    $urlRouterProvider.otherwise("/");
+  }
+
+  function CandidateFactory($resource){
+    var Candidate = $resource("/api/candidates/:name", {}, {
+      update: {method: "PUT"}
+    });
+    Candidate.all = Candidate.query();
+    return Candidate;
+  } // candidate factory
+
+  function candIndexCtrl(CandidateFactory){
+    var vm = this;
+    vm.candidates = CandidateFactory.all;
+  }
 
 })();
