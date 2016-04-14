57c1b01fe8ad15c9dc25e24565daba4e2f1f6b13
Charles <charlesthwang@gmail.com>
2016-04-13 23:26:20 -0400

    inclass code & set value for isCurrentUser for all candidates and display candidates on index.view

--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,54 @@
 "use strict";
 
 (function(){
-  
+
   angular
   .module("candidates", [
     "ui.router",
     "ngResource"
-  ]);
+  ])
+  .config([
+    "$stateProvider",
+    "$locationProvider",
+    "$urlRouterProvider",
+    Router
+  ])
+  .factory("CandidateFactory", [
+    "$resource",
+    CandidateFactory
+  ])
+  .controller("CandidateIndexController", [
+    "CandidateFactory",
+    CandidateIndexController
+  ])
+
+  function Router($stateProvider, $locationProvider, $urlRouterProvider){
+    $locationProvider.html5Mode(true);
+    $stateProvider
+      .state("welcome", {
+        url: "/",
+        templateUrl: "/assets/html/app-welcome.html"
+      })
+      .state("index", {
+        url: "/candidates",
+        templateUrl: "/assets/html/candidates-index.html",
+        controller: "CandidateIndexController",
+        controllerAs: "indexVM"
+      })
+      $urlRouterProvider.otherwise("/")
+  }
+
+  function CandidateFactory($resource){
+    var CandidateFactory = $resource("/api/candidates/:name", {}, {
+      update: {method: "PUT"}
+    });
+    CandidateFactory.all = CandidateFactory.query();
+    return CandidateFactory;
+  }
+
+  function CandidateIndexController(CandidateFactory){
+    var vm = this;
+    vm.candidates = CandidateFactory.all;
+  }
 
 })();
