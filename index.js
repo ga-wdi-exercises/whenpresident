e32d38c1568b8aee9020236b004c8d71842ae1be
yinkaagiri <yinka.fatai@gmail.com>
2016-04-14 05:51:47 -0400

    adds post and body parser

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
-var express = require("express");
-var hbs     = require("express-handlebars");
-var db      = require("./db/connection");
+var express  = require("express");
+var hbs      = require("express-handlebars");
+var mongoose = require("./db/connection");
+
+var parser = require("body-parser");
+
+var Candidate = mongoose.model("Candidate");
 
 var app     = express();
 
 app.set("port", process.env.PORT || 3001);
+
 app.set("view engine", "hbs");
 app.engine(".hbs", hbs({
   extname:        ".hbs",
@@ -12,28 +17,40 @@ app.engine(".hbs", hbs({
   layoutsDir:     "views/",
   defaultLayout:  "layout-main"
 }));
+
 app.use("/assets", express.static("public"));
+app.use(parser.urlencoded({extended: true}));
 
 app.get("/", function(req, res){
   res.render("app-welcome");
 });
 
 app.get("/candidates", function(req, res){
-  res.render("candidates-index", {
-    candidates: db.candidates
+  Candidate.find({}).then(function(candidates) {
+    res.render("candidates-index", {
+      candidates: candidates
+    });
   });
 });
 
 app.get("/candidates/:name", function(req, res){
   var desiredName = req.params.name;
-  var candidateOutput;
-  db.candidates.forEach(function(candidate){
-    if(desiredName === candidate.name){
-      candidateOutput = candidate;
-    }
+  Candidate.findOne({name: desiredName}).then(function(candidate) {
+    res.render("candidates-show", {
+      candidate: candidate
+    });
   });
-  res.render("candidates-show", {
-    candidate: candidateOutput
+});
+
+app.post("/candidates", function(req, res) {
+  Candidate.create(req.body.candidate).then(function() {
+    res.redirect("/candidates");
+  });
+});
+
+app.post("/candidates/:name", function(req, res) {
+  Candidate.findOneAndUpdate(req.body.candidate).then(function(candidate) {
+    res.redirect("/candidates/" + candidate.name);
   });
 });
 
