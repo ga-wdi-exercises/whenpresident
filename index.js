ded81471ff18ae4c787b4f41879a363e39836423
Ryan Krolick <rkrolick@gmail.com>
2016-04-13 20:14:10 -0400

    added edit and delete functionality

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 var express = require("express");
-var hbs     = require("express-handlebars");
-var db      = require("./db/connection");
+var parser = require("body-parser");
+var hbs = require("express-handlebars");
+var mongoose = require("./db/connection");
 
-var app     = express();
+var app = express();
 
 app.set("port", process.env.PORT || 3001);
 app.set("view engine", "hbs");
@@ -13,27 +14,46 @@ app.engine(".hbs", hbs({
   defaultLayout:  "layout-main"
 }));
 app.use("/assets", express.static("public"));
+app.use(parser.urlencoded({extended: true}));
+
+var Candidate = mongoose.model("Candidate");
 
 app.get("/", function(req, res){
   res.render("app-welcome");
 });
 
 app.get("/candidates", function(req, res){
-  res.render("candidates-index", {
-    candidates: db.candidates
+  Candidate.find().then(function(c){
+    res.render("candidates-index", {
+      candidates: c
+    });
+  });
+});
+
+app.post("/candidates", function(req, res){
+  //res.json(req.body); NOTE: See the body of the request as json
+  Candidate.create(req.body.candidate).then(function(){
+    res.redirect("/candidates");
   });
 });
 
 app.get("/candidates/:name", function(req, res){
-  var desiredName = req.params.name;
-  var candidateOutput;
-  db.candidates.forEach(function(candidate){
-    if(desiredName === candidate.name){
-      candidateOutput = candidate;
-    }
+  Candidate.findOne({name: req.params.name}).then(function(c){
+    res.render("candidates-show",{
+      candidate: c
+    });
   });
-  res.render("candidates-show", {
-    candidate: candidateOutput
+});
+
+app.post("/candidates/:name/delete", function(req, res){
+  Candidate.remove({name: req.params.name}).then(function(){
+    res.redirect("/candidates");
+  });
+});
+
+app.post("/candidates/:name/update", function(req, res){
+  Candidate.findOneAndUpdate({name: req.params.name}, req.body.candidate).then(function(){
+    res.redirect("/candidates");
   });
 });
 
