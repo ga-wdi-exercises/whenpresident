f3e5aecd00f2277058e2dcf5fbafc9b268cd62ce
YOUR FULL NAME <farishta.haider@gmail.com>
2016-04-14 00:54:47 -0400

    can't remember which branch I did the hw on

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 var express = require("express");
-var parser  = require("body-parser");
-var hbs     = require("express-handlebars");
-var mongoose= require("./db/connection");
+var parser = require("body-parser");
+var hbs = require("express-handlebars");
+var session = require("express-session");
+var request = require("request");
+var qstring = require("qs");
+var cmongo = require("connect-mongo");
+var twitter = require("./lib/twitter_auth");
+var mongoose = require("./db/connection");
 
-var app     = express();
+var app = express();
 
 var Candidate = mongoose.model("Candidate");
 
@@ -15,9 +20,49 @@ app.engine(".hbs", hbs({
   layoutsDir:     "views/",
   defaultLayout:  "layout-main"
 }));
+
+if(process.env.NODE_ENV !== "production"){
+  var env   = require("./env");
+  process.env.session_secret = env.session_secret;
+  process.env.t_callback_url = env.t_callback_url;
+  process.env.t_consumer_key = env.t_consumer_key;
+  process.env.t_consumer_secret = env.t_consumer_secret;
+}
+
+
+app.use(session({
+  secret: process.env.session_secret,
+  resave: false,
+  saveUninitialized: false,
+  store: new (require("connect-mongo")(session))({
+    mongooseConnection: mongoose.connection
+  })
+}));
+
+
 app.use("/assets", express.static("public"));
 app.use(parser.urlencoded({extended: true}));
+app.use(function(req, res, next){
+  twitter.checkIfSignedIn(req, res, function(){
+    next();
+  });
+});
 
+app.get("/", function(req, res){
+  res.render("candidates");
+});
+
+app.get("/login/twitter", function(req, res){
+  twitter.getSigninURL(req, res, function(url){
+    res.redirect(url);
+  });
+});
+
+app.get("/login/twitter/callback", function(req, res){
+  twitter.whenSignedIn(req, res, function(){
+    res.redirect("/");
+  });
+});
 app.get("/", function(req, res){
   res.render("app-welcome");
 });
@@ -46,15 +91,16 @@ app.post("/candidates", function(req, res){
 
 app.post("/candidates/:name/delete", function(req, res){
   Candidate.findOneAndRemove({name: req.params.name}).then(function(){
-    res.redirect("/candidates")
+    res.redirect("/candidates");
   });
 });
 
 app.post("/candidates/:name", function(req, res){
-  Candidate.findOneAndUpdate({name: req.params.name}, req.body.candidate, {new: true}).then(function(candidate){
+  Candidate.findOneAndUpdate( {name: req.params.name},req.body.candidate).then(function(){
     res.redirect("/candidates/" + candidate.name);
   });
 });
+
 
 app.post("/candidates/:name/positions", function(req, res){
   Candidate.findOne({name: req.params.name}).then(function(candidate){
@@ -65,8 +111,8 @@ app.post("/candidates/:name/positions", function(req, res){
   });
 });
 
-app.post("/candidates/:name/positions/:index", function(req, res){
-  Candidate.findOne({name: req.params.name}).then(function(candidate){
+app.post("/candidates/:name/positions/index", function(req, res){
+  Candidate.findOne( {name: req.params.name}).then(function(candidate){
     candidate.positions.splice(req.params.index, 1);
     candidate.save().then(function(){
       res.redirect("/candidates/" + candidate.name);
