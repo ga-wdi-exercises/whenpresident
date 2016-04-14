1b5b98e996bdba56db630ccdd17311b0acbf9561
Lyndsey Newsome <lyndseysnewsome@gmail.com>
2016-04-14 06:36:02 -0400

    commit

--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,25 @@
 "use strict";
 
 (function(){
-  
+
   angular
   .module("candidates", [
     "ui.router",
     "ngResource"
-  ]);
-
+  ])
+  .config([
+    "$locationProvider",
+    Router
+  ])
+function Router($stateProvider, $locationProvider){
+  $locationProvider.html5Mode(true); ..
+    .state("welcome",{
+      url:"/",
+      templateUrl: "/assets/html/candidates-welcome.html"
+    })
+  .state("index",{
+    url:"/candidates",
+    template: "<h2>This is the candidates index page.</h2>"
+  });
+ }
 })();
