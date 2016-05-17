ab1cccbf91ce8139300d92276da348281e3b149e
RobertAKARobin <robertgfthomas@gmail.com>
2016-05-17 08:34:10 -0400

    Added router and some test states

--- public/js/app.js
+++ public/js/app.js
@@ -5,5 +5,19 @@
   .module("whenpresident", [
     "ui.router",
     "ngResource"
-  ]);
+  ])
+  .config(Router);
+  
+  Router.$inject = ["$stateProvider"];
+  function Router($stateProvider){
+    $stateProvider
+    .state("index", {
+      url: "/",
+      template: "This is the index page"
+    })
+    .state("show", {
+      url: "/:name",
+      template: "This is the show page"
+    });
+  }
 })();
