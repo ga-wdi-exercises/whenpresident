58567564a7e45793c74d6d03eba99ba0d45afac2
RobertAKARobin <robertgfthomas@gmail.com>
2016-05-17 08:42:52 -0400

    Displays fake candidate data on index

--- public/js/app.js
+++ public/js/app.js
@@ -6,18 +6,40 @@
     "ui.router",
     "ngResource"
   ])
-  .config(Router);
+  .config(Router)
+  .controller("Index", Index);
   
   Router.$inject = ["$stateProvider"];
   function Router($stateProvider){
     $stateProvider
     .state("index", {
       url: "/",
-      templateUrl: "/assets/html/candidates-index.html"
+      templateUrl:  "/assets/html/candidates-index.html",
+      controller:   "Index",
+      controllerAs: "IndexVM"
     })
     .state("show", {
       url: "/:name",
       template: "This is the show page"
     });
+  }
+  
+  Index.$inject = [];
+  function Index(){
+    var vm = this;
+    vm.candidates = [
+      {
+        "name": "Steve",
+        "year": 2024
+      },
+      {
+        "name": "Barry",
+        "year": 2008
+      },
+      {
+        "name": "Dubya",
+        "year": 2000
+      }
+    ];
   }
 })();
